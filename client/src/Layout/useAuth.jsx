0cfc18803a828000eff00101da25f80a0505f1e7
Bryant1356 <bryantv0.2.0@gmail.com>
2023-07-07 21:41:17 +0700

    Auth

---
+++ client/src/Layout/useAuth.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+export const useAuth = (code) => {
+    const [accessToken, setAccessToken] = useState();
+    const [refreshToken, setRefreshToken] = useState();
+    const [expiresIn, setExpiresIn] = useState();
+
+    useEffect(() => {
+        axios.post('http://localhost:3001/login', {
+            code,
+        }).then(res => {
+            console.log(res.data)
+        }).catch(() => {
+            window.location = '/'
+        })
+    }, [code]);
+}
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const spotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new spotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '2c42c0211f4548e3a419a2bef902f2cf',
+        clientSecret: '68ed835e5e774af89f5ad46c83e95888'
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        });
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(400)
+    })
+})
+
+app.listen(3001)
