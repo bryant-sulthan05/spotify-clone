8ccd8d00147afa7d0d21ede26abcd8368e0562bc
Bryant1356 <bryantv0.2.0@gmail.com>
2023-07-08 21:06:37 +0700

    refresh token

--- server/server.js
+++ server/server.js
@@ -8,22 +8,42 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new spotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '2c42c0211f4548e3a419a2bef902f2cf',
+        clientSecret: '68ed835e5e774af89f5ad46c83e95888',
+        refreshToken
+    })
+
+    spotifyApi.refreshAccessToken().then(
+        (data) => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn
+            })
+        }).catch(err => {
+            console.log(err)
+            res.sendStatus(400)
+        })
+})
+
 app.post('/login', (req, res) => {
     const code = req.body.code;
     const spotifyApi = new spotifyWebApi({
         redirectUri: 'http://localhost:3000',
         clientId: '2c42c0211f4548e3a419a2bef902f2cf',
-        clientSecret: '68ed835e5e774af89f5ad46c83e95888'
+        clientSecret: '68ed835e5e774af89f5ad46c83e95888',
     })
 
     spotifyApi.authorizationCodeGrant(code).then(data => {
         res.json({
             accessToken: data.body.access_token,
             refreshToken: data.body.refresh_token,
-            expiresIn: data.body.expires_in
-        });
+            expiresIn: data.body.expires_in,
+        })
     }).catch((err) => {
-        console.log(err);
         res.sendStatus(400)
     })
 })
